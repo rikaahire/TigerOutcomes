217eb9e0bab50f3bdc97dbd80842bcf90fa329ae
vl7131 <vl7131@princeton.edu>
2024-10-31 18:39:53 -0400

    Vitus: Search by major scaffolding

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from shiny import reactive, render
 from shiny.express import input, ui
 
 income_range = 1
-
+degree = "string"
 
 # Add page title and sidebar
 ui.page_opts(title="TigerOutcomes", fillable=True)
 
 with ui.sidebar(open="desktop"):
-    ui.input_action_button("search", "Search")
+    ui.input_text("major_search", "Major:", ""),
+    ui.input_action_button("search", "Search", class_="btn-success"),
 
 with ui.layout_columns(fill=False):
     with ui.value_box():
         "Income Range",
         income_range
+
+    with ui.value_box():
+        "Degree",
+        degree
