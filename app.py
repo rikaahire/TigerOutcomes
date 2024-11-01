60e15a498253680ddec2adebdb7537b4c91fd40e
vl7131 <vl7131@princeton.edu>
2024-11-01 13:49:26 -0400

    Vitus: More frontend changes

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from shiny import reactive, render
 from shiny.express import input, ui
 import prototype
 
+
+
 income_range = 1
 degree = "string"
-values = prototype.get_values()[1]
+values = prototype.get_values(input.major_search)
+
+print(values)
 
 # Add page title
 ui.page_opts(title="TigerOutcomes", fillable=True)
@@ -12,21 +16,32 @@ ui.page_opts(title="TigerOutcomes", fillable=True)
 # Add sidebar
 with ui.sidebar(open="desktop"):
     ui.input_text("major_search", "Major:", ""),
-    "You entered:"
-    @render.text(inline=True)  
-    def text():
-        return input.major_search()
     ui.input_action_button("search", "Search", class_="btn-success"),
 
 with ui.layout_columns(fill=False):
     with ui.value_box():
-        "Income Range",
-        income_range
-
-    with ui.value_box():
-        "value",
-        degree
-
-    with ui.value_box():
         "Degree",
-        degree
+        @render.text(inline=True)  
+        def text():
+            return input.major_search()
+
+    with ui.value_box():
+        "First Gen",
+        @render.text(inline=True)  
+        def value_txt():
+            return values[2]
+        
+    with ui.value_box():
+        "Income",
+        @render.text(inline=True)  
+        def income_txt():
+            return values[3]
+    
+    with ui.value_box():
+        "Legal Sex",
+        @render.text(inline=True)  
+        def sex_txt():
+            return values[4]
+        
+
+    
--- prototype.py
+++ prototype.py
@@ -1,9 +1,11 @@
 import database as db
 
-def get_values():
-    x = db.get_rows("demographics", 16000)
-    legal_sex = x[:,1]
-    first_gen = x[:,2]
-    income = x[:,3]
-    major = x[:,5]
-    return major
+def get_values(major_in):
+    # x = db.get_rows("demographics", 16000)
+    # legal_sex = x[:,1]
+    # first_gen = x[:,2]
+    # income = x[:,3]
+    # major = x[:,5]
+    out = [0, 1, 2, 3, "Female"]
+    print(major_in)
+    return out
