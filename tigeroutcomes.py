a5538317f388c6e6ac96fab9e350a2c89b9024e1
Bryan Zhang <bz5989@princeton.edu>
2024-11-11 14:27:13 -0500

    Bryan: Cleaned up app.py/connected html
    Removed comments from tigeroutcomes;
    Connected home page to default;
    Modified to be sticky header instead of fixed header;
    Added a few more linkings

--- tigeroutcomes.py
+++ tigeroutcomes.py
@@ -16,7 +16,23 @@ from top import app
 #-----------------------------------------------------------------------
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
-def reg_overviews():
+@app.route('/home', methods=['GET'])
+def home():
+
+    # auth.authenticate()
+    try:
+        html_code = flask.render_template('homepage.html')
+    except Exception as ex:
+        print(ex)
+        html_code = flask.render_template('servererror.html')
+
+    response = flask.make_response(html_code)
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/search', methods=['GET'])
+def search():
 
     # auth.authenticate()
     # major = flask.request.args.get('major')
@@ -34,62 +50,26 @@ def reg_overviews():
 
 #-----------------------------------------------------------------------
 
+@app.route('/landing', methods=['GET'])
+def landing():
+
+    # auth.authenticate()
+    try:
+        html_code = flask.render_template('landingpage.html')
+    except Exception as ex:
+        print(ex)
+        html_code = flask.render_template('servererror.html')
+
+    response = flask.make_response(html_code)
+    return response
+
+#-----------------------------------------------------------------------
+
 @app.route('/job', methods=['GET'])
-def reg_details():
+def job_details():
 
     # auth.authenticate()
     html_code = flask.render_template('job.html')
     response = flask.make_response(html_code)
 
     return response
-
-
-# from shiny import reactive, render
-# from shiny.express import input, ui
-# import prototype
-# import os
-
-# secret_key = os.environ['APP_SECRET_KEY']
-
-# income_range = 1
-# degree = "string"
-# values = []
-
-# # Add page title
-# ui.page_opts(title="TigerOutcomes", fillable=True)
-
-# # Add sidebar
-# with ui.sidebar(open="desktop"):
-#     ui.input_text("major_search", "Major:", "Computer Science"),
-#     ui.input_action_button("search", "Search", class_="btn-success"),
-
-# with ui.layout_columns(fill=False):
-#     with ui.value_box():
-#         "Degree",
-#         @render.text(inline=True)  
-#         def text():
-#             return input.major_search()
-
-#     with ui.value_box():
-#         "First Gen",
-#         @render.text(inline=True)  
-#         def value_txt():
-#             values = prototype.get_values(input.major_search())            
-#             return values[2]
-        
-#     with ui.value_box():
-#         "Income",
-#         @render.text(inline=True)  
-#         def income_txt():
-#             values = prototype.get_values(input.major_search())
-#             return "$" + str (values[3])
-    
-#     with ui.value_box():
-#         "Degree Type",
-#         @render.text(inline=True)  
-#         def sex_txt():
-#             values = prototype.get_values(input.major_search())
-#             return values[4]
-        
-
-    
