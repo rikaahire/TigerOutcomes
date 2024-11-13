f6301d70567a74a7481880bd313c2f3efcecac73
Bryan Zhang <bz5989@princeton.edu>
2024-11-13 14:57:34 -0500

    Bryan: Added json connections
    Now, searches and modals can reference json queries, like in A4

--- tigeroutcomes.py
+++ tigeroutcomes.py
@@ -2,6 +2,7 @@
 
 import flask
 import database as db
+import json
 # import auth
 from top import app
 # import dotenv
@@ -31,6 +32,21 @@ def home():
 
 #-----------------------------------------------------------------------
 
+@app.route('/landing', methods=['GET'])
+def landing():
+
+    # auth.authenticate()
+    try:
+        html_code = flask.render_template('landingpage.html')
+    except Exception as ex:
+        print(ex)
+        html_code = flask.render_template('servererror.html')
+
+    response = flask.make_response(html_code)
+    return response
+
+#-----------------------------------------------------------------------
+
 @app.route('/search', methods=['GET'])
 def search():
 
@@ -49,30 +65,35 @@ def search():
     response = flask.make_response(html_code)
     return response
 
-#-----------------------------------------------------------------------
-
-@app.route('/landing', methods=['GET'])
-def landing():
-
-    # auth.authenticate()
-    try:
-        html_code = flask.render_template('landingpage.html')
-    except Exception as ex:
-        print(ex)
-        html_code = flask.render_template('servererror.html')
-
-    response = flask.make_response(html_code)
+@app.route('/results', methods=['GET'])
+def results():
+    major = flask.request.args.get('major')
+    if major is None:
+        major = ''
+        major = major.strip()
+    results = db.get_rows("pton_demographics", "AcadPlanDescr", major)
+    results = [tuple(row) for row in results]
+    # will want to change results to a major -> job call
+    json_doc = json.dumps(results)
+    response = flask.make_response(json_doc)
+    response.headers['Content-Type'] = 'application/json'
     return response
 
 #-----------------------------------------------------------------------
 
 @app.route('/job', methods=['GET'])
 def job_details():
-
+    soc_code = flask.request.args.get('soc_code')
+    if soc_code is None:
+        return []
     # auth.authenticate()
-    descript = db.get_occupational_data_full("11-1011.00")
-    html_code = flask.render_template('job.html', description=descript[0], 
-                                      skills=descript[1], knowledge=descript[2])
-    response = flask.make_response(html_code)
+    descript = db.get_occupational_data_full(soc_code)
+    descript = [tuple(row) for row in descript]
+    json_doc = json.dumps(descript)
+    response = flask.make_response(json_doc)
+    response.headers['Content-Type'] = 'application/json'
+    # html_code = flask.render_template('job.html', description=descript[0], 
+    #                                   skills=descript[1], knowledge=descript[2])
+    # response = flask.make_response(html_code)
 
     return response
